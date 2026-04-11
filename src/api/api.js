3e6808a1c66b1275b0d68e0e988dcc4dfbc2a9e0
Navya <23eg112b37@anurag.edu.in>
2026-04-11 23:37:07 +0530

    frontend ready

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const api = axios.create({ baseURL: "/api" })
+const api = axios.create({ baseURL: "https://event-backend-vxkn.onrender.com/api" })
 let _auth = null
 
 export function setBasicAuth(u, p) {
